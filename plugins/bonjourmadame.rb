6503da563dad7ae34a9a44bb6e3f9e3e34dcdcab
Franco Bouly <fbouly@gmail.com>
2012-06-07 12:56:50 +0200

    BonjourMadame now understand random and help parameter

--- plugins/bonjourmadame.rb
+++ plugins/bonjourmadame.rb
@@ -8,13 +8,24 @@ module Plugins
     include Cinch::Plugin
     set :react_on, :channel
 
-    match /!bonjourmadame/i, method: :bonjourmadame
-    def bonjourmadame(m)
+    match /!(?:bonjourmadame|bjmd)(?: -?([rlh]|help))?/i, method: :bonjourmadame
+    def bonjourmadame(m,param)
       feed = Nokogiri::XML(open(BONJOURMADAME))
-      sub = feed.css("channel > item").first
-      m.reply "Bonjour Madame - %s" % [ 
-        sub.css("link").inner_text.to_s
-      ]
+
+      case param
+        when nil,'l'
+          sub = feed.css("channel > item").first
+        when 'h','help'
+          m.reply "!bjmd [-param] : Les parametres possibles sont r (random) ou l (last)"
+        else
+          subA = feed.css("channel > item").to_a
+          sub = subA.sample
+      end
+      if (defined? sub)
+        m.reply "Bonjour Madame - %s" % [ 
+          sub.css("link").inner_text.to_s
+        ]
+      end
     end
 
   end
